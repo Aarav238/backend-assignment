c6f8c35293b546bf03e707372582d0b961183e19
Aarav 238 <aarav8090shukla@gmail.com>
2024-07-28 01:30:44 +0530

    validations

---
+++ middlewares/validation.js
@@ -0,0 +1,53 @@
+import Joi from "joi";
+
+export const validateRegistration = (req, res, next) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    name: Joi.string().required(),
+    mobile: Joi.string().required(),
+    password: Joi.string().min(5).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  next();
+};
+
+export const validateLogin = (req, res, next) => {
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().required()
+    });
+  
+    const { error } = schema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    next();
+  };
+
+
+
+  export const validateExpense = (req, res, next) => {
+    const schema = Joi.object({
+      amount: Joi.number().positive().required(),
+      description: Joi.string().required(),
+      splitMethod: Joi.string().valid('equal', 'exact', 'percentage').required(),
+      splits: Joi.array().items(Joi.object({
+        user: Joi.string().required(),
+        amount: Joi.number().when('splitMethod', {
+          is: 'exact',
+          then: Joi.required()
+        }),
+        percentage: Joi.number().when('splitMethod', {
+          is: 'percentage',
+          then: Joi.required().min(0).max(100)
+        })
+      })).required()
+    });
+  
+    const { error } = schema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    next();
+  };
