5e44df085f14f3b9a50abe5ad14e1552f2f5d200
Aarav 238 <aarav8090shukla@gmail.com>
2024-07-27 23:42:38 +0530

    models

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+
 
 app.get("/", (req, res) => {
     res.send("hello world");
 });
 
-app.listen(5000, () => {
-    console.log("server running on port 5000");
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ models/Expense.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const expenseSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  payer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  splitMethod: {
+    type: String,
+    enum: ["equal", "exact", "percentage"],
+    required: true,
+  },
+  splits: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      amount: Number,
+      percentage: Number,
+    },
+  ],
+});
+
+
+const Expense = mongoose.model("Expense", expenseSchema);
