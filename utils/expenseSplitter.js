407c63575d9f13cf930fd93907987b0dfc7da29c
Aarav 238 <aarav8090shukla@gmail.com>
2024-07-28 20:49:29 +0530

    calculateBalanceSheet

--- utils/expenseSplitter.js
+++ utils/expenseSplitter.js
@@ -27,3 +27,37 @@ export const calculateSplits = (amount, splitMethod, splits) => {
       throw new Error("Invalid split method");
   }
 };
+
+
+
+
+
+
+export const calculateBalanceSheet = (expenses, userId) => {
+    const balanceSheet = {};
+  
+    expenses.forEach(expense => {
+      if (expense.payer.toString() === userId.toString()) {
+        // Current user paid for this expense
+        expense.splits.forEach(split => {
+          if (split.user.toString() !== userId.toString()) {
+            if (!balanceSheet[split.user]) balanceSheet[split.user] = 0;
+            balanceSheet[split.user] += split.amount;
+          }
+        });
+      } else {
+        // Current user owes money for this expense
+        const userSplit = expense.splits.find(split => split.user.toString() === userId.toString());
+        if (userSplit) {
+          if (!balanceSheet[expense.payer]) balanceSheet[expense.payer] = 0;
+          balanceSheet[expense.payer] -= userSplit.amount;
+        }
+      }
+    });
+  
+    return Object.entries(balanceSheet).map(([user, amount]) => ({
+      user,
+      amount: parseFloat(amount.toFixed(2)),
+      type: amount > 0 ? 'to receive' : 'to pay'
+    }));
+  };
